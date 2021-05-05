e78d43bb7db0cf93330577ebe80ae33b8ca4ce09
Usarneme <jamestcoop@gmail.com>
2021-05-05 10:02:15 -0700

    creates first sanity check test for creating class instance
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

--- PrimeSifting.Tests/ModelTests/PrimeSiftingTest.cs
+++ PrimeSifting.Tests/ModelTests/PrimeSiftingTest.cs
@@ -1,1 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeSifting.Test
+{
+  [TestClass]
+  public class PrimeSiftingTests
+  {
+    [TestMethod]
+    public void SieveOfEratosthenes_Exists_True()
+    {
+      // arrange, act, assert
+      SieveOfEratosthenes _mySieve = new SieveOfEratosthenes();
+      Assert.IsInstanceOfType(_mySieve, typeof(SieveOfEratosthenes));
+    }
+  }
+}
